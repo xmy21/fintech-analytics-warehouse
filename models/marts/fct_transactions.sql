1266efe652933ee2a96dbfd95482912256ce7142
Ming Yamamoto <xmshammin@gmail.com>
2026-07-23 22:54:20 +0100

    Convert fct_transactions to incremental materialization with merge strategy

--- models/marts/fct_transactions.sql
+++ models/marts/fct_transactions.sql
@@ -1,13 +1,16 @@
 {{
     config(
-        materialized='table',
+        materialized='incremental',
+        unique_key='transaction_id',
         partition_by={
             "field": "transaction_date",
             "data_type": "date"
         },
-        cluster_by=["account_id"]
+        cluster_by=["account_id"],
+        incremental_strategy='merge'
     )
 }}
+
 select
     t.transaction_id,
     t.account_id,
@@ -23,3 +26,7 @@ select
 from {{ ref('stg_transactions') }} t
 left join {{ ref('stg_accounts') }} a
     on t.account_id = a.account_id
+
+{% if is_incremental() %}
+where t.transaction_at > (select max(transaction_at) from {{ this }})
+{% endif %}
